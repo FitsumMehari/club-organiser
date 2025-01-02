5ee0e2f288aa71ea4d2a9e85c3bde649cb6bd207
FitsumMehari <m.fitsummehari@gmail.com>
2025-01-02 19:16:59 +0300

    create DB schema for users

---
+++ backend/models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    userType: {
+        type: String,
+        enum: ["organiser", "admin"],
+        default: "organiser",
+        required: true,
+    },
+    clubs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "club",
+        required: false,
+    }],
+    generatedOTP: {
+        type: String,
+        required: false,
+    },
+    profilePic: {
+        type: String,
+        required: false,
+    },
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model("user", UserSchema);
