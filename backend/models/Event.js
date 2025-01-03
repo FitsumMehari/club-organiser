35e2c9ec66b5e9190308fa70c256863638de1c38
FitsumMehari <m.fitsummehari@gmail.com>
2025-01-03 09:02:29 +0300

    create Event DB schema

---
+++ backend/models/Event.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const EventSchema = new mongoose.Schema({
+    logo: {
+        type: String,
+        required: false,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        enum: ["undefined", "social", "academic", "management"],
+        default: "undefined",
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    organiser: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "club",
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: false,
+    },
+    attendees: [{
+        name: String,
+        email: String,
+    }, ],
+    status: {
+        type: String,
+        enum: ["open", "closed"],
+    },
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model("event", EventSchema);
