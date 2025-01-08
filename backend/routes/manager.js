0b0e3b772b93a30f77380d0351fbc314aa8904ff
FitsumMehari <m.fitsummehari@gmail.com>
2025-01-08 18:19:55 +0300

    add route to approve membership requests

--- backend/routes/manager.js
+++ backend/routes/manager.js
@@ -6,6 +6,7 @@ dotenv.config();
 const Club = require("../models/Club");
 const { verifyToken } = require("./verifyToken");
 
+const transporter = require("./transporter")
 
 
 // Get Single Club By ID Of Manager
@@ -23,5 +24,41 @@ router.get("/club", verifyToken, async(req, res, next) => {
         }
     }
 });
+
+// Update A CLub By Club ID When A Request To Be A Member is Accepted By The Club Managers
+router.put("/club/approvemembership/:clubID", verifyToken, async(req, res, next) => {
+    if (req.params.clubID) {
+        try {
+            const club = await Club.findById(req.params.clubID);
+
+            const mailOptions = {
+                from: process.env.OTP_EMAIL,
+                to: req.body.email,
+                subject: "Membership Request",
+                text: `Your request to join ${club.name} club has been approved by the managers.`,
+            };
+
+            const pendingRequest = club.members.find(member => member.email === req.body.email);
+            if (pendingRequest) {
+                pendingRequest.status = "accepted"
+                await club.save()
+                await transporter.sendMail(mailOptions);
+                return res.status(400).json({ message: 'Membership request approved!' });
+            }
+
+
+            res.status(400).json({
+                message: "Request not found!",
+            });
+        } catch (error) {
+            next(error);
+        }
+    } else {
+        res.status(400).json({ message: "Invalid club ID!" });
+
+    }
+});
+
+
 
 module.exports = router;
