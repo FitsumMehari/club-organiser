8bd4556f91df77d25a8f126e39c1160f30dbd005
FitsumMehari <m.fitsummehari@gmail.com>
2025-01-08 18:25:43 +0300

    use verifyOTP function from its own file

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,8 +3,8 @@ const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
-const nodemailer = require("nodemailer");
 const transporter = require("./transporter")
+const verifyOTP = require("./verifyOTP")
 
 // const jwtPrivateKey = fs.readFileSync('./rsa.pem', 'utf8');
 const jwtPrivateKey = process.env.JWTKEY;
@@ -202,28 +202,6 @@ function generateOTP() {
     return otp;
 }
 
-
-
-// Verify OTP Route
-const verifyOTP = async(req, res, next) => {
-    const { email, otp } = req.body;
-
-    try {
-        const existingUser = await User.findOne({ email });
-        if (!existingUser) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        if (existingUser.generatedOTP !== otp) {
-            return res.status(400).json({ message: "Invalid or expired OTP" });
-        }
-
-        // res.json({ message: 'OTP verified' });
-        next();
-    } catch (error) {
-        res.status(500).json({ message: "Error verifying OTP" });
-    }
-};
 
 router.post("/reset-password", verifyOTP, async(req, res) => {
     const { email, newPassword } = req.body;
