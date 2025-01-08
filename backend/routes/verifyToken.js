9c743252e2fdf4a86e3125f9ac5d7e0ca64fad8c
FitsumMehari <m.fitsummehari@gmail.com>
2025-01-08 19:22:14 +0300

    use authorization instead of token in req.headers to receive the token from

--- backend/routes/verifyToken.js
+++ backend/routes/verifyToken.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const verifyToken = (req, res, next) => {
-    const authHeader = req.headers.token;
+    const authHeader = req.headers.authorization;
     if (!authHeader) {
         return res.status(401).json({
             "error": {
