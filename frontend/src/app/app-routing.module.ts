67c3ac9bf068fa1eab62475da4bd7e74add772fb
FitsumMehari <m.fitsummehari@gmail.com>
2025-01-17 21:01:06 +0300

    add sample routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,12 +8,14 @@ const routes: Routes = [
   {path: "clubs", component: AppComponent },
   {path: "events", component: AppComponent },
   {path: "contact", component: AppComponent },
-  {path: "register", component: AppComponent },
+  {path: "join-club", component: AppComponent },
+  {path: "reserve-ticket", component: AppComponent },
+  {path: "send-proposal", component: AppComponent },
   {path: "login", component: AppComponent },
-  {path: "user", component: AppComponent },
   {path: "forgot-password", component: AppComponent },
   {path: "verify-otp", component: AppComponent },
   {path: "reset-password", component: AppComponent },
+  {path: "user", component: AppComponent },
 ];
 
 @NgModule({
