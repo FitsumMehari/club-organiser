300efce1aebea0a4cee20bd61cf5a783a98a5a69
FitsumMehari <m.fitsummehari@gmail.com>
2025-01-18 16:54:54 +0300

    finish clubs page

--- frontend/src/app/pages/clubs/clubs.component.ts
+++ frontend/src/app/pages/clubs/clubs.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-clubs',
   templateUrl: './clubs.component.html',
-  styleUrl: './clubs.component.css'
+  styleUrl: './clubs.component.css',
 })
 export class ClubsComponent {
-
+  clubs = [
+    { image: 'image6.svg', id: '1' },
+    { image: 'image7.svg', id: '2' },
+    { image: 'image6.svg', id: '3' },
+    { image: 'image7.svg', id: '4' },
+    { image: 'image6.svg', id: '1' },
+    { image: 'image7.svg', id: '2' },
+    { image: 'image6.svg', id: '3' },
+    { image: 'image7.svg', id: '4' },
+  ];
 }
