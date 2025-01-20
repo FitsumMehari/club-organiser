7be657b57e23dfe4e263b78d2587391baa8af900
FitsumMehari <m.fitsummehari@gmail.com>
2025-01-20 11:06:16 +0300

    make the navbar and footer visible only if user is not logged in

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthGuardService } from './guards/route.guard';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'club-organiser';
-
+  constructor(private authGuard: AuthGuardService) {}
+  isAuthorized: boolean = false
+  ngOnInit(): void {
+      this.isAuthorized = this.authGuard.canActivate()
+  }
 }
