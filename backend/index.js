a51f51c57b7b76f3d173c6eef4218fe07f71c50a
FitsumMehari <m.fitsummehari@gmail.com>
2025-01-23 17:55:35 +0300

    added built app to backend

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,16 @@ dotenv.config();
 
 const app = express();
 
-// app.use(express.static("browser"));
+app.use(express.static("browser"));
 
 app.use(cors()); // Use the cors middleware with your options
 
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("index.html");
+});
 
 const authRoute = require("./routes/auth");
 const clubRoute = require("./routes/club");
@@ -39,8 +43,8 @@ app.use("/proposals", proposalRoute);
 
 //ROUTE NOT FOUND
 app.use((req, res, next) => {
-    res.status(404).send("Sorry, route could not be located!");
-    // res.redirect("/");
+    // res.status(404).send("Sorry, route could not be located!");
+    res.redirect("/");
 });
 
 //ERROR
