0346d8471375f423ca60a3e6d88a0cb76139bb51
FitsumMehari <m.fitsummehari@gmail.com>
2025-01-27 12:03:15 +0300

    add filtering

--- frontend/src/app/pages/clubs/clubs.component.ts
+++ frontend/src/app/pages/clubs/clubs.component.ts
@@ -10,12 +10,27 @@ import { Club } from '../../club.interface';
 export class ClubsComponent implements OnInit {
   constructor(private clubsService: ClubsService) {}
 
-  allClubs:any = []
+  allClubs: any = [];
+  filterInput: any;
 
   ngOnInit(): void {
     this.clubsService.getAllClubs();
-    this.clubsService._response.subscribe((next) => {
-      this.allClubs = next;
+    // this.clubsService._response.subscribe((next) => {
+    //   this.allClubs = next;
+    // });
+    this.setClubs()
+  }
+
+   filterClubs(category: any) {
+    // this.clubsService.getAllClubs();
+
+     this.setClubs()
+    this.allClubs = this.allClubs.filter((club:any) => club.category === category)
+  }
+
+   setClubs() {
+     this.clubsService._response.subscribe( (next) => {
+       this.allClubs = next;
     });
   }
 }
